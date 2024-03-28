8e5227b6fda0982b07d15fff894e634b6c771d4e
Rishita Sharma <rishita4477@gmail.com>
2024-03-28 18:04:21 +0530

    leetcode 2958

---
+++ Leetcode2958.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+public class Leetcode2958 {
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 1, 2, 3, 1, 2 };
+        int k = 2;
+        System.out.println(longestsubarray(arr, k));
+
+    }
+
+    public static int longestsubarray(int nums[], int k) {
+        int ans = 0;
+        int start = -1;
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        for (int end = 0; end < nums.length; end++) {
+            freq.put(nums[end], freq.getOrDefault(nums[end], 0) + 1); // putting the values of array into map and
+                                                                      // calculating frequency
+            while (freq.get(nums[end]) > k) {
+                start++;
+                freq.put(nums[start], freq.get(nums[start]) - 1); // shrinking the window
+            }
+            ans = Math.max(ans, end - start); // for calculating longest subarray, growing the window
+        }
+        return ans;
+    }
+}
